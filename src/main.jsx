da4e8e7516d882bd657b9d6940d66cea77dca080
daynefarley-art <dayne.farley@gmail.com>
2025-09-10 16:49:48 +1200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css"; // <-- crucial
+import "./index.css"; // <-- this line is crucial
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
